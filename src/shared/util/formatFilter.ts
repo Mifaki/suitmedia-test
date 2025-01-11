89d7c32d04e51d10b24cf40c68c1b8c7a04c177a
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2025-01-11 13:33:04 +0700

    fix(bug): change pageSize default value

--- src/shared/util/formatFilter.ts
+++ src/shared/util/formatFilter.ts
@@ -3,7 +3,7 @@ import type { TGeneralFilter } from '../models/generalinterfaces';
 export const formatFilter = (query: TGeneralFilter): Record<string, string> => {
   const queryParams: Record<string, string> = {
     'page[number]': String(Math.max(1, query.page ?? 1)),
-    'page[size]': String(Math.max(1, query.pageSize ?? 10)),
+    'page[size]': String(Math.max(10, query.pageSize ?? 10)),
     sort: query.sort ?? '-published_at',
   };
 
